f6f2598610460f7b4a32f22999b89be75d2fded1
Charles X. Morrissey <me@charles-x.com>
2018-04-30 21:35:10 -0400

    change app to root

--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@ import ReactDOM from 'react-dom'
 import App from '@/components/App'
 import '@/assets/css/app.css'
 
-ReactDOM.render(<App />, document.getElementById('app'))
+ReactDOM.render(<App />, document.getElementById('root'))
