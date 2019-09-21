d5517017608fa594af6dc0358f78d2cff2eca097
Charles X. Morrissey <me@charles-x.com>
2019-09-21 09:55:32 -0400

    package maintence

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { render } from 'react-dom'
 import App from '@/components/App'
 import '@/assets/css/base.css'
 
-ReactDOM.render(<App />, document.getElementById('app'))
+render(<App />, document.getElementById('app'))
