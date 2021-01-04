cf0194b66ffba2816eb16603c48b854a3ef7703c
Charles X. Morrissey <me@charles-x.com>
2021-01-03 21:54:16 -0500

    add postcss-custom-properties and postcss-preset-env

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,17 @@
 module.exports = {
   plugins: {
-    autoprefixer: {
-      flexbox: 'no-2009',
+    'postcss-custom-properties': {
+      importFrom: './src/assets/css/base.css',
+      preserve: false,
+    },
+    'postcss-preset-env': {
+      autoprefixer: {
+        flexbox: 'no-2009',
+      },
+      stage: 3,
+      features: {
+        'custom-properties': false,
+      },
     },
   },
 }
